b7357c4a8a4e686d2f3b0e582085001ae78fe457
xiaoran <wxrbw@qq.com>
2017-06-01 18:16:57 +0800

    docs($location): 添加配置文件等

--- routes/api.js
+++ routes/api.js
@@ -1,3 +1,13 @@
-/**
- * Created by wuxiaoran on 2017/6/1.
- */
+var express = require('express');
+var router = express.Router();
+
+/* GET users listing. */
+router.get('/tours', function(req, res, next) {
+  const tours = [
+    { id: 0, name: 'Hood River', price: 99.99 },
+    { id: 1, name: 'Oregon Coast', price: 149.95 },
+  ];
+  res.json(tours);
+});
+
+module.exports = router;
--- routes/index.js
+++ routes/index.js
@@ -8,8 +8,39 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 router.get('/about', function(req, res, next) {
+  res.render('about', {
+    title: 'about',
+    text: 'about me',
+  });
+});
+
+router.get('/about/:userId', function(req, res, next) {
   res.type('text/plain');
-  res.send('About Web');
+  console.log(req.params, req.query, req.body,
+    req.xhr, req.url);
+  res.send('about');
+});
+
+router.get('/headers', function(req, res, next) {
+  res.type('text/plain');
+  let s = '';
+  for (let k in req.headers) {
+    s += `${k}:${req.headers[k]}\n`;
+  }
+  res.send(s);
+});
+
+router.get('/test_Pug', function(req, res, next) {
+  const data = {
+    currency: { name: 'United States dollars', abbrev: 'USD'},
+    tours: [
+      { name: 'Hood River', price: '$99.95' },
+      { name: 'Oregon Coast', price: '$159.95'}
+    ],
+    specialsUrl: '/january-specials',
+    currencies: [ 'USD', 'GBP', 'BTC' ],
+  }
+  res.render('test', {...data});
 });
 
 module.exports = router;

86b1df6844bf3e6150b59a5ff6c21485c72eef46
xiaoran <wxrbw@qq.com>
2017-06-05 18:44:51 +0800

    try post

--- routes/index.js
+++ routes/index.js
@@ -50,6 +50,16 @@ router.get('/home', function(req, res, next) {
   res.locals.partials.weather = getWeatherData();
   res.render('home');
 });
+router.get('/newsletter', function (req, res){
+  // 我们会在后面学到 CSRF……目前，只提供一个虚拟值
+  res.render('news_letter', { csrf: 'CSRF token goes here' });
+});
+app.post('/process', function (req, res){
+  console.log('Form (from querystring): ' + req.query.form);
+  console.log('CSRF token (from hidden form field): ' + req.body._csrf);
+  console.log('Name (from visible form field): ' + req.body.name);
+  console.log('Email (from visible form field): ' + req.body.email);
+  res.redirect(303, '/welcome'); });
 
 function getWeatherData(){
   return {
